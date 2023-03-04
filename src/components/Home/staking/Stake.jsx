9c5374b091923e7ca34d1ea32e1d0429dd2d07ff
Nicholas Murasko <nickmura@Nicholass-MacBook-Air.local>
2023-03-03 17:52:53 -0700

    Fucking fuck

--- src/components/Home/staking/Stake.jsx
+++ src/components/Home/staking/Stake.jsx
@@ -4,7 +4,7 @@ import DasboardButton from '../../components/ui/DasboardButton';
 
 import Modal from '@/components/ui/Modal';
 const Stake = ({
-  titile,
+  title,
   StakingAPR,
   MaxSlashing,
   WalletBalance,
@@ -20,10 +20,10 @@ const Stake = ({
       <div className="">
         <div className="flex  py-5  gap-9 ">
           <h2 className="text-[1.500em] leading-[148%] text-[#333333]">
-            Stake {titile}
+            Stake {title}
           </h2>
           <span className="bg-[#DEFFD8] flex items-center justify-center rounded-[21px] py-1 px-3">
-            <span className="text-skin-green">Buy {titile} with FIAT</span>
+            <span className="text-skin-green">Buy {title} with FIAT</span>
           </span>
         </div>
         <hr className="h-[3px] w-full px-3 bg-black opacity-30" />
@@ -32,7 +32,7 @@ const Stake = ({
         <div className="basis-[45%] border-r border-slate-400 pr-2">
           <div className="logo items-center gap-3 flex ">
             <Image src={moneda} alt="moneda" />
-            <h2 className="text-[18px]">{titile}</h2>
+            <h2 className="text-[18px]">{title}</h2>
           </div>
           <ul className="flex w-full flex-col gap-3 mt-6">
             <li className="flex gap-10 justify-between">
@@ -63,7 +63,7 @@ const Stake = ({
         <Modal toggleDrawer={toggleDrawer} isOpen={isOpen} />
         <div className="basis-[55%] text-center ">
           <div className="flex flex-col gap-2">
-            <h2 className="text-lg text-skin-lightDark">Staked {titile}</h2>
+            <h2 className="text-lg text-skin-lightDark">Staked {title}</h2>
             <h2 className="text-2xl text-[#666666]">{totalStaked}</h2>
             <p className="text-[#333333] text-base">Cooldown Period</p>
             <h4 className="text-xs text-skin-muted">{CooldownPeriod}</h4>
@@ -73,12 +73,12 @@ const Stake = ({
           </div>
           <hr className="h-[3px] w-full my-6 px-3 bg-black opacity-30" />
           <div className="flex flex-col gap-2">
-            <h2 className="text-lg text-skin-lightDark">Claimable {titile}</h2>
+            <h2 className="text-lg text-skin-lightDark">Claimable {title}</h2>
             <h2 className="text-2xl text-[#666666]">{totalClaimble}</h2>
-            <p className="text-[#333333] text-base">{titile} per month</p>
+            <p className="text-[#333333] text-base">{title} per month</p>
             <h4 className="text-xs text-skin-muted">{cliamPerMonth}</h4>
             <button className="py-2 px-3 rounded-[5px] border border-skin-green text-skin-green">
-              Claim {titile}
+              Claim {title}
             </button>
           </div>
         </div>
